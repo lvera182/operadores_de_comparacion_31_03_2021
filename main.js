361afeb37bbc0bb9849051aebdb5bfb2e65d2096
lvera182 <lvera182@gmail.com>
2021-03-31 20:34:31 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+let a = 5;
+let b = '5';
+
+console.log(a, typeof(a));
+console.log(b, typeof(b));
+
+console.warn("Menor");
+console.log(a < b); 
+
+console.warn("Menor o igual");
+console.log(a <= b); 
+
+console.warn("Mayor");
+console.log(a > b); 
+
+console.warn("Mayor o igual");
+console.log(a >= b); 
+
+console.warn("Igualdad");
+console.log(a == b); 
+
+console.warn("Identico");
+console.log(a === b); 
+
+console.warn("No Igualdad");
+console.log(a != b); 
+
+console.warn("No Identico");
+console.log(a !== b); 
+
